0724f3c80bdd7dfcc3789dcb487f83a2f4dd6bac
robomantis19 <freefavors@gmail.com>
2019-12-18 15:24:42 -0500

    added UpdateMovie and route

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+
+
+const UpdateMovie = () => { 
+    const [form, setForm] = ({
+        director: "",
+        id: undefined, 
+        metascore: undefined, 
+        stars: [],
+        title: ""
+    })
+
+    return (
+        <div>
+            update movie
+        </div>
+    )
+}
+
+export default UpdateMovie
